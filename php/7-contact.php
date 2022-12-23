cc379face54c0a4f6d60ce670336b65a045ceb3b
llucarz <leo.lucarz@caensup.fr>
2022-12-23 16:38:40 +0100

    V0.2

--- php/7-contact.php
+++ php/7-contact.php
@@ -23,23 +23,3 @@
         </form>
     </div>
 </section>
-
-<?php
-    if(!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['email']) && !empty($_POST['message'])){
-        $firstname = htmlspecialchars($_POST['firstname'])
-        $lastname = htmlspecialchars($_POST['lastname'])
-        $email = htmlspecialchars($_POST['email'])
-        $message = htmlspecialchars($_POST['message'])
-
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-        }
-        else {
-            echo 'Email non valide'
-        }
-    }
-    else {
-        heade('Location:index.php')
-        die()
-    }
-?>
